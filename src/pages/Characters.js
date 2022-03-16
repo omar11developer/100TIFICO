6d213412c19ff71ae8491a21aff900c6154ec3df
Omar Menjivar <omarmenjivar083@gmail.com>
2022-03-15 19:50:16 -0600

    add template and pages

---
+++ src/pages/Characters.js
@@ -0,0 +1,21 @@
+const Characters = () => {
+const view = `
+    <div class="characters-inner">
+        <articule class="characters-card">
+            <img src="image" alt="name">
+            <h2>Name</h2>
+        </articule>
+        <articule class="characters-card">
+            <h3>Episodes: </h3>
+            <h3>Status: </h3>
+            <h3>Species: </h3>
+            <h3>Gener: </h3>
+            <h3>Origin: </h3>
+            <h3>Last Location: </h3>
+        </articule>
+    </div>
+`;
+return view;
+
+}
+export default Characters;
